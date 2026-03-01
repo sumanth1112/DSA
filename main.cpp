36a8d3124a7aec8999965cd391610d19e26c77c5
sumanth1112 <thimmareddysumanthreddy@gmail.com>
2026-03-01 17:34:16 +0530

    first commit

---
+++ main.cpp
@@ -0,0 +1,42 @@
+// #include <bits/stdc++.h> 
+// using namespace std;
+
+// int main() {                                 *           
+//     int n = 5;                               * *
+//     for(int i=0;i<n;i++) {                   * * *
+//         for(int j=0;j<=i;j++) {              * * * *
+//             cout<<"*"<<" ";                  * * * * *
+//         }
+//         cout<<endl;
+//     }
+// }
+
+
+
+// #include <bits/stdc++.h> 
+// using namespace std;
+
+// int main() {                                     * * * * *
+//     int n = 5;                                   * * * *
+//     for(int i=0;i<n;i++) {                       * * *
+//         for(int j=n-1;j>=i;j--) {                * *
+//             cout<<"*"<<" ";                      *
+//         }
+//         cout<<endl;
+//     }
+// }
+
+
+
+#include <bits/stdc++.h> 
+using namespace std;
+
+int main() {
+    int n = 5;
+    for(int i=0;i<n;i++) {
+        for(int j=n-1;j>=i;j--) {
+            cout<<"*"<<" ";
+        }
+        cout<<endl;
+    }
+}
